29ec5e4b0fe4fd660de871356c3f48ec7e347094
Kevin Gutowski <kgutowski@berkeley.edu>
2018-06-09 17:20:30 -0700

    Move colors into separate JS file

---
+++ src/colors.js
@@ -0,0 +1,11 @@
+export var Colors = {
+    c1: "#FF3B30", //red
+    c2: "#FF9500", //orange
+    c3: "#FFCC00", //yellow
+    c4: "#D8FF00", // lime-green
+    c5: "#4CD964", //green
+    c6: "#5AC8FA", //teal blue
+    c7: "#007AFF", //blue
+    c8: "#5856D6", //purple
+    c9: "#FF2D55" //pink
+}
--- src/my-command.js
+++ src/my-command.js
@@ -1,21 +1,17 @@
+import { Colors } from './Colors'
+
 const Document = require('sketch/dom').Document
 const Style = require('sketch/dom').Style
 var doc
 var selection
-var color1 = "#1ca9c4"
-var color2 = "#FF3B30" //red
-var color3 = "#FF9500" //orange
-var color4 = "#FFCC00" //yellow
-var color5 = "#4CD964" //green
-var color6 = "#5AC8FA" //teal blue
-var color7 = "#007AFF" //blue
-var color8 = "#5856D6" //purple
-var color9 = "#FF2D55" //pink
 
 function setup(context) {
   doc = Document.getSelectedDocument()
   selection = doc.selectedLayers
-  context.document.showMessage('Plugin 🏃')
+  // context.document.showMessage('Plugin 🏃')
+  if (selection.length == 0) {
+    context.document.showMessage('🗝🌈: Please select a layer')
+  }
 }
 
 // ****************************
@@ -24,92 +20,92 @@ function setup(context) {
 
 export function applyFillColor1(context) {
   setup(context)
-  applyFillColorWithSelection(color1, selection)
+  applyFillColorWithSelection(Colors.c1, selection)
 }
 
 export function applyFillColor2(context) {
   setup(context)
-  applyFillColorWithSelection(color2, selection)
+  applyFillColorWithSelection(Colors.c2, selection)
 }
 
 export function applyFillColor3(context) {
   setup(context)
-  applyFillColorWithSelection(color3, selection)
+  applyFillColorWithSelection(Colors.c3, selection)
 }
 
 export function applyFillColor4(context) {
   setup(context)
-  applyFillColorWithSelection(color4, selection)
+  applyFillColorWithSelection(Colors.c4, selection)
 }
 
 export function applyFillColor5(context) {
   setup(context)
-  applyFillColorWithSelection(color5, selection)
+  applyFillColorWithSelection(Colors.c5, selection)
 }
 
 export function applyFillColor6(context) {
   setup(context)
-  applyFillColorWithSelection(color6, selection)
+  applyFillColorWithSelection(Colors.c6, selection)
 }
 
 export function applyFillColor7(context) {
   setup(context)
-  applyFillColorWithSelection(color7, selection)
+  applyFillColorWithSelection(Colors.c7, selection)
 }
 
 export function applyFillColor8(context) {
   setup(context)
-  applyFillColorWithSelection(color8, selection)
+  applyFillColorWithSelection(Colors.c8, selection)
 }
 
 export function applyFillColor9(context) {
   setup(context)
-  applyFillColorWithSelection(color9, selection)
+  applyFillColorWithSelection(Colors.c9, selection)
 }
 
 export function applyBorderColor1(context) {
   setup(context)
-  applyBorderColorWithSelection(color1, selection);
+  applyBorderColorWithSelection(Colors.c1, selection);
 }
 
 export function applyBorderColor2(context) {
   setup(context)
-  applyBorderColorWithSelection(color2, selection);
+  applyBorderColorWithSelection(Colors.c2, selection);
 }
 
 export function applyBorderColor3(context) {
   setup(context)
-  applyBorderColorWithSelection(color3, selection);
+  applyBorderColorWithSelection(Colors.c3, selection);
 }
 
 export function applyBorderColor4(context) {
   setup(context)
-  applyBorderColorWithSelection(color4, selection);
+  applyBorderColorWithSelection(Colors.c4, selection);
 }
 
 export function applyBorderColor5(context) {
   setup(context)
-  applyBorderColorWithSelection(color5, selection);
+  applyBorderColorWithSelection(Colors.c5, selection);
 }
 
 export function applyBorderColor6(context) {
   setup(context)
-  applyBorderColorWithSelection(color6, selection);
+  applyBorderColorWithSelection(Colors.c6, selection);
 }
 
 export function applyBorderColor7(context) {
   setup(context)
-  applyBorderColorWithSelection(color7, selection);
+  applyBorderColorWithSelection(Colors.c7, selection);
 }
 
 export function applyBorderColor8(context) {
   setup(context)
-  applyBorderColorWithSelection(color8, selection);
+  applyBorderColorWithSelection(Colors.c8, selection);
 }
 
 export function applyBorderColor9(context) {
   setup(context)
-  applyBorderColorWithSelection(color9, selection);
+  applyBorderColorWithSelection(Colors.c9, selection);
 }
 
 function applyFillColorWithSelection(color, selection) {
@@ -119,7 +115,8 @@ function applyFillColorWithSelection(color, selection) {
       color: color,
     }]
   })
-  // TODO: Support layers within Groups
+    // TODO: Support layers within Groups
+  doc.sketchObject.inspectorController().reload();
 }
 
 function applyBorderColorWithSelection(color, selection) {
@@ -134,4 +131,5 @@ function applyBorderColorWithSelection(color, selection) {
     }]
   })
   // TODO: Support layers within Groups
+  doc.sketchObject.inspectorController().reload();
 }
